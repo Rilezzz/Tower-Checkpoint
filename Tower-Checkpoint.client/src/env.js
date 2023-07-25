c39d653fbbe04e7632f5031f0536eaacf667673b
Riley <riley@idahosuperiorcustoms.com>
2023-07-25 16:38:48 -0600

    Init

---
+++ Tower-Checkpoint.client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'dev-h30qdrnxw3fag5mj.us.auth0.com'
+export const clientId = 'aYbeoXrXuRl54Cyznkvc2MO9W9lSvaf9'
+export const audience = 'classroom.com'
